cfd2c26c13b023b58d89c4e927b9e05481216f95
Emily Aguirre <emgal.aguirre@gmail.com>
2020-12-10 19:22:42 -0500

    created resume-card

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -6,6 +6,11 @@ function Resume() {
     <Wrapper>
       <h4 className="text-center header">Resume</h4>
       <p className="text-center">Check out some of my relevant experience! </p>
+      <div className="card resume-card mt-10">
+        <div className="card-body">
+          <h5 className="card-title p-1">Education</h5>
+        </div>
+      </div>
     </Wrapper>
   );
 }
