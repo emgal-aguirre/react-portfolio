cf9df39ba95e99487894d3bc9ad089e4c1800647
Emily Aguirre <emgal.aguirre@gmail.com>
2020-12-12 18:44:38 -0500

    creat portfolio card

--- portfolio/src/pages/projects.js
+++ portfolio/src/pages/projects.js
@@ -3,7 +3,7 @@ import Wrapper from '../components/wrapper';
 import { Card } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 
-function Projects() {
+const Projects = () => {
   const projectInfo = [
     {
       image:
@@ -11,7 +11,12 @@ function Projects() {
       title: 'Emily',
       text: 'Here is my text',
     },
-    { image: '', title: 'Carlos', text: 'Here is my text' },
+    {
+      image:
+        'https://500px.com/photo/1016968550/Circumambulation-by-Raimondo-Jereb/',
+      title: 'Carlos',
+      text: 'Here is my text',
+    },
     { image: '', title: 'Mackenzie', text: 'Here is my text' },
     { image: '', title: 'Nate', text: 'Here is my text' },
     { image: '', title: 'Luisa', text: 'Here is my text' },
@@ -20,24 +25,23 @@ function Projects() {
 
   const renderCard = (card, index) => {
     return (
-      <Wrapper>
-        <h4 className="text-center header">Projects</h4>
-        <p className="text-center">Check out some of my projects down below!</p>
-        <Card style={{ width: '18rem' }} key={index}>
-          <Card.Img variant="top" src="holder.js/100px180" src={card.image} />
-          <Card.Body>
-            <Card.Title>{card.title}</Card.Title>
-            <Card.Text>{card.text}</Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-      </Wrapper>
+      <Card style={{ width: '18rem' }} key={index}>
+        <Card.Img variant="top" src="holder.js/100px180" src={card.image} />
+        <Card.Body>
+          <Card.Title>{card.title}</Card.Title>
+          <Card.Text>{card.text}</Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
     );
-
-    return (
-      {projectInfo.map(renderCard)}
-    )
   };
-}
+  return (
+    <Wrapper>
+      <h4 className="text-center header">Projects</h4>
+      <p className="text-center">Check out some of my projects down below!</p>
+      <div>{projectInfo.map(renderCard)}</div>;
+    </Wrapper>
+  );
+};
 
 export default Projects;
