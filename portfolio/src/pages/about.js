492e946957e9f9e3ac16a2829ab89b7de27e763f
Emily Aguirre <emgal.aguirre@gmail.com>
2020-12-14 15:08:27 -0500

    updated about page, not formatted

--- portfolio/src/pages/about.js
+++ portfolio/src/pages/about.js
@@ -1,38 +1,47 @@
 import React from 'react';
 import Wrapper from '../components/wrapper';
+import '../App.css';
 import { Card } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-solid-svg-icons';
 import Button from 'react-bootstrap/Button';
 import Box from '../components/box.css';
 
-const About = () => {
-  const cardInfo = [
-    {
-      image: 'portfolio/src/Assets/images/recipe-square.jpg',
-      title: 'Emily Aguirre',
-      description: 'Front-End Developer',
-      link: 'https://emgal-aguirre.github.io/Recipe-Generator/',
-      github: 'https://github.com/emgal-aguirre/Recipe-Generator',
-    },
-  ];
-
-  const renderCard = (card, index) => {
-    return (
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>{card.title}</Card.Title>
-          <Card.Text>{card.description}</Card.Text>
-          <Card.Link href="#">Card Link</Card.Link>
-          <Card.Link href="#">Another Link</Card.Link>
-        </Card.Body>
-      </Card>
-    );
-  };
+function About() {
   return (
     <Wrapper>
-      <div className="grid">{cardInfo.map(renderCard)}</div>;
+      <container class="about">
+        <div className="row about-row">
+          <div className="col-6">
+            left
+            <Card class="bio-card" style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title class="name headshot-name">
+                  Emily <br></br>Aguirre
+                </Card.Title>
+                <hr class="solid-blue" />
+                <Card.Subtitle class="mb-2 text-muted title">
+                  FRONT END DEVELOPER
+                </Card.Subtitle>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="col-6">
+            right
+            <h1 class="about-text">Hello</h1>
+            <Button class="btn btn-primary resume-btn">RESUME</Button>
+            <Button class="btn btn-outline-dark project-btn">PROJECTS</Button>
+            <p>
+              Hello, I'm Emily, a Front-end Developer and designer based in
+              Austin, Texas.
+              <br />
+              <br />I enjoy developing beautiful and functional websites.
+            </p>
+          </div>
+        </div>
+      </container>
     </Wrapper>
   );
-};
+}
 
 export default About;
